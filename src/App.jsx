cf50f0b431d5b40a766057afcea8b5198f730685
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-11-25 15:43:10 +0300

    Update App component to include PlayerInfo for player display

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import PlayerInfo from "./components/PlayerInfo";
+
 function App() {
-  return <p>helloo</p>;
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <PlayerInfo name={"Player 1"} symbol={"X"} />
+          <PlayerInfo name={"Player 2"} symbol={"O"} />
+        </ol>
+        GAME BOARD
+      </div>
+      LOGS
+    </main>
+  );
 }
 
 export default App;
